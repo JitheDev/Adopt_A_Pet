bbbf74864d0f3c4ac5f1c3f074dbcf32a0262c58
Ji <Jikang.dev@gmail.com>
2023-09-17 15:30:38 -0400

    remove unusued varibale

--- src/pages/petDetailsNotFound/index.js
+++ src/pages/petDetailsNotFound/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import useNavigate here.
-import Navigation from '../../components/navigation';
+
 import { useNavigate } from 'react-router-dom';
 
 const PetDetailsNotFound = () => {
